41a3914d2d82e91c5186f78b8ed3c8e891578bae
Eugene Khizhnyak <chernomor1984@gmail.com>
2018-07-03 03:43:20 +0300

    Init

---
+++ GBShop/GBShop/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  GBShop
+//
+//  Created by Eugene Khizhnyak on 03.07.2018.
+//  Copyright © 2018 Chernomor. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
